15ca3d74709433bda3445831fabfc863f084f0cd
hg.pyun <hg.pyun@linecorp.com>
2016-10-12 22:20:32 +0900

    add comment

--- src/momo.js
+++ src/momo.js
@@ -3,10 +3,15 @@ const bodyParser = require('body-parser');
 
 var app = express();
 
+// set SSL
+
 app.use(bodyParser.json());
 
 app.post('/call', function (request, response, next) {
     console.log(request.body);
+
+    // business logic
+
     response.json({'result' : 'success'});
 });
 
